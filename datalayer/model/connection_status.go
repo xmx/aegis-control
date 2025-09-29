5536eb57e5a050ce538ee584d57e0ee3d6b61948
wzy <hnwzy@live.com>
2025-09-29 19:03:55 +0800

    文件 FS 修改

--- datalayer/model/connection_status.go
+++ datalayer/model/connection_status.go
@@ -5,6 +5,7 @@ import "time"
 type ConnectionStatus struct {
 	ConnectedAt    time.Time `json:"connected_at,omitzero"    bson:"connected_at,omitempty"`
 	DisconnectedAt time.Time `json:"disconnected_at,omitzero" bson:"disconnected_at,omitempty"`
+	KeepaliveAt    time.Time `json:"keepalive_at,omitzero"    bson:"keepalive_at,omitempty"`
 	Protocol       string    `json:"protocol,omitzero"        bson:"protocol,omitempty"`
 	Subprotocol    string    `json:"subprotocol,omitzero"     bson:"subprotocol,omitempty"`
 	RemoteAddr     string    `json:"remote_addr,omitzero"     bson:"remote_addr,omitempty"`
