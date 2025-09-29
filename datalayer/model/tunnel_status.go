dd9332ea8ca76eec28a3da6fbb06aba63505787c
wzy <hnwzy@live.com>
2025-09-29 19:04:56 +0800

    文件 FS 修改

--- datalayer/model/tunnel_status.go
+++ datalayer/model/tunnel_status.go
@@ -2,7 +2,7 @@ package model
 
 import "time"
 
-type ConnectionStatus struct {
+type TunnelStatus struct {
 	ConnectedAt    time.Time `json:"connected_at,omitzero"    bson:"connected_at,omitempty"`
 	DisconnectedAt time.Time `json:"disconnected_at,omitzero" bson:"disconnected_at,omitempty"`
 	KeepaliveAt    time.Time `json:"keepalive_at,omitzero"    bson:"keepalive_at,omitempty"`
