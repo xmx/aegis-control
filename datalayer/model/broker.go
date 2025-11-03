2d0de619b4dfb8d3c9df5943ec7c965da71d2de6
wzy <hnwzy@live.com>
2025-11-03 19:44:31 +0800

    规范化认证

--- datalayer/model/broker.go
+++ datalayer/model/broker.go
@@ -9,7 +9,7 @@ import (
 type Broker struct {
 	ID          bson.ObjectID `json:"id,omitzero"              bson:"_id,omitempty"`          // ID
 	Name        string        `json:"name"                     bson:"name"`                   // 名字
-	Secret      string        `json:"secret"                   bson:"secret,omitempty"`       // 连接密钥
+	Secret      string        `json:"-"                        bson:"secret,omitempty"`       // 连接密钥
 	Status      bool          `json:"status"                   bson:"status,omitempty"`       // 状态
 	Config      BrokerConfig  `json:"config,omitempty"         bson:"config,omitempty"`       // 配置
 	Networks    NodeNetworks  `json:"networks,omitzero"        bson:"networks,omitempty"`     // 网卡设备
